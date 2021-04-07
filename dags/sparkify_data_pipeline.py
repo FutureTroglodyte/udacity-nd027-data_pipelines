d8df7e5893a4364603fcb56b1fd5ebab1a3491ef
FutureTroglodyte <future.troglodyte@posteo.org>
2021-04-07 23:54:31 +0200

    Adjusted the schedule of the data pipeline DAG.

--- dags/sparkify_data_pipeline.py
+++ dags/sparkify_data_pipeline.py
@@ -15,14 +15,15 @@ from helpers import SqlQueries
 
 default_args = {
     "owner": "udacity",
-    "start_date": datetime(2019, 1, 12),
+    # "start_date": datetime(2019, 1, 12),
+    "start_date": datetime.now(),
 }
 
 dag = DAG(
-    "udac_example_dag",
+    "sparkify_data_pipeline",
     default_args=default_args,
     description="Load and transform data in Redshift with Airflow",
-    schedule_interval="0 * * * *",
+    schedule_interval="@once",
 )
 
 start_operator = DummyOperator(task_id="Begin_execution", dag=dag)
@@ -52,3 +53,11 @@ load_time_dimension_table = LoadDimensionOperator(
 run_quality_checks = DataQualityOperator(task_id="Run_data_quality_checks", dag=dag)
 
 end_operator = DummyOperator(task_id="Stop_execution", dag=dag)
+
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+load_songplays_table >> [
+    load_user_dimension_table,
+    load_song_dimension_table,
+    load_artist_dimension_table,
+    load_time_dimension_table
+] >> run_quality_checks >> end_operator
