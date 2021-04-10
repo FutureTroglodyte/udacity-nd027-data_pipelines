795666a7f1c50d54a5b6a9888911632957c2508f
FutureTroglodyte <future.troglodyte@posteo.org>
2021-04-10 15:30:15 +0200

    Added load fact and load dimension operators.

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -2,21 +2,31 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class LoadDimensionOperator(BaseOperator):
 
-    ui_color = '#80BD9E'
+class LoadDimensionOperator(BaseOperator):
+    """
+    Inserts data into dim table, given the tables's name & a select statement.
+
+    Does truncate the table at first.
+    """
+
+    ui_color = "#80BD9E"
 
     @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
-
+    def __init__(
+        self, redshift_conn_id="", table="", select_statement="", *args, **kwargs
+    ):
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_statement = select_statement
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run(f"TRUNCATE TABLE {self.table}")
+
+        self.log.info("Loading dimension table")
+        redshift.run(f"INSERT INTO {self.table} " + self.select_statement)
+        self.log.info("Done!")
--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -2,21 +2,31 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class LoadFactOperator(BaseOperator):
 
-    ui_color = '#F98866'
+class LoadFactOperator(BaseOperator):
+    """
+    Inserts data into fact table, given the tables's name & a select statement.
+
+    Does not (!) truncate the table at first.
+    """
+
+    ui_color = "#F98866"
 
     @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
-
+    def __init__(
+        self, redshift_conn_id="", table="", select_statement="", *args, **kwargs
+    ):
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_statement = select_statement
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        # self.log.info("Clearing data from destination Redshift table")
+        # redshift.run(f"TRUNCATE TABLE {self.table}")
+
+        self.log.info("Inserting Data into Fact Table")
+        redshift.run(f"INSERT INTO {self.table} " + self.select_statement)
+        self.log.info("Done!")
